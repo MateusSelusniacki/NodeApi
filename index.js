f44d0c044bff81b76a750ca56117d9740db4a14c
unknown <mat-coelho@live.com>
2022-08-04 10:13:47 -0300

    Projeto API

---
+++ index.js
@@ -0,0 +1,20 @@
+const os = require('os')
+const log = require('./logger')
+
+setInterval(() => {
+  const { freemem, totalmem } = os
+
+  const total = parseInt(totalmem() / 1024 / 1024)
+  const mem = parseInt(freemem() / 1024 / 1024)
+  const percents = parseInt((mem / total) * 100)
+
+  const stats = {
+    free: `${mem}`,
+    total: `${total}`,
+    usage: `${percents}%`
+  }
+  console.clear()
+  console.table(stats)
+
+  log(`${JSON.stringify(stats)} \n`)
+}, 1000)
